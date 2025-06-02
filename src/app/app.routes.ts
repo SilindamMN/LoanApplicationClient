0193564b1a7f8cb3aa5b2e29ae534e7dbc3a43fa
silindamn <silindanemdu@gmail.com>
2025-06-02 23:07:20 +0200

    layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { BankerListComponent } from './pages/banker-list/banker-list.component';
+import { CustomerListComponent } from './pages/customer-list/customer-list.component';
+import { LoanApplicationListComponent } from './pages/loan-application-list/loan-application-list.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NewLoanFormComponent } from './pages/new-loan-form/new-loan-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'banker-list', component: BankerListComponent },
+    { path: 'customer-list', component: CustomerListComponent },
+    { path: 'loan-application-list', component: LoanApplicationListComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'new-loan-form', component: NewLoanFormComponent },
+    { path: '**', redirectTo: 'login' } // wildcard for unknown paths
+];
+
+
+
